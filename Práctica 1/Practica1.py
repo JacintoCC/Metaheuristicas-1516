dc76bcda9046d61a6852e6601795699e74cd0140
JacintoCC <jacintocc0@gmail.com>
2016-03-13 18:42:40 +0100

    First version SFS

--- Práctica 1/Practica1.py
+++ Práctica 1/Practica1.py
@@ -1,3 +1,6 @@
 from scipy.io import arff
+from algorithms import *
 
 datos = arff.loadarff('Datos/wdbc.arff')[0]
+
+print(runSFS(datos))
--- Práctica 1/algorithms.py
+++ Práctica 1/algorithms.py
@@ -1,9 +1,10 @@
 from math import sqrt
 import numpy as np
+from random import *
 
 def distance(x,y):
     dist = 0
-    for i in range(0,len(x)):
+    for i in range(len(x)):
         dist = dist + (x[i]-y[i])*(x[i]-y[i])
     return sqrt(dist)
 
@@ -16,6 +17,7 @@ def countingVotes(mins, categories):
             return categories[i]
 
 def kNN(k, data, categories, item):
+    # ¡Por aquí! 3
     distances =  [[i, distance(data[i],item)] for i in range(len(data))]
     mins = []
 
@@ -26,13 +28,11 @@ def kNN(k, data, categories, item):
 
     return countingVotes(mins, categories)
 
-def greedySFS(training_data, evaluating_data):
+def greedySFS(training_data):
     train_categ = [row[0] for row in training_data]
-    eval_categ = [row[0] for row in evaluating_data]
 
     num_features = len(training_data[0])-1
-    train_subj =  [row[1:(num_features+1)] for row in training_data]
-    eval_subj =  [row[1:(num_features+1)] for row in evaluating_data]
+    train_subj =  [[row[i] for i in range(1,num_features+1)] for row in training_data]
 
     solution = [0 for i in range(num_features)]
     profit_v = [0 for i in range(num_features)]
@@ -40,11 +40,12 @@ def greedySFS(training_data, evaluating_data):
     previous_profit = 0
 
     while(exists_profit):
-        for feat in range(len(profit_v)) if solution[feat]==0:
-            train_feats_i = [[row[j] for j in range(num_features) if (j==feat or solution[j]==1)] for row in training_data]
-            for i in range(len(eval_subj))
-                item = [eval_subj[i][j] for j in range(num_features) if (j==feat or solution[j]==1)]
-                correct = (kNN(3,train_feats_i, train_categ, item) == eval_categ[i])
+        for feat in [i for i in range(len(profit_v)) if solution[i]==0]:
+            for i in range(len(train_subj)):
+                train_feats = [[train_subj[j][k] for k in range(num_features) if (k==feat or solution[k]==1)] for j in range(len(train_subj)) if j != i]
+                item = [train_subj[i][j] for j in range(num_features) if (j==feat or solution[j]==1)]
+                # ¡Por aquí! 2
+                correct = (kNN(3,train_feats, train_categ, item) == train_categ[i])
                 if(correct):
                     profit_v[feat] += 1
         current_profit = max(profit_v)
@@ -55,5 +56,29 @@ def greedySFS(training_data, evaluating_data):
     return solution
 
 def runSFS(data, iterations = 10):
+    corrects_vector = []
+
     for i in range(iterations):
-        
+        rnd_subject = sample(range(len(data)),len(data)//2)
+        training_data = [data[j] for j in rnd_subject ]
+
+        eval_data = [[data[j][k] for k in range(1,len(data[0]))] for j in range(len(data)) if not j in rnd_subject]
+        eval_categ = [data[j][0] for j in range(len(data)) if not j in rnd_subject]
+
+        # ¡Por aquí 1!
+        solution = greedySFS(training_data)
+        train_categ = [row[0] for row in training_data]
+        corrects = 0
+
+        for j in range(len(eval_data)):
+            train_feats = [[row[j] for j in solution if j==1] for row in training_data]
+            if(kNN(3,train_feats, train_categ, eval_data[i]) == eval_categ[i]):
+                corrects += 1
+
+        corrects_vector.append(corrects)
+
+    mean = 0
+    for n in corrects:
+        mean += n
+
+    return mean/(iterations*len(eval_data)*1.0)*100.0
